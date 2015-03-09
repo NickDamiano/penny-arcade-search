1745a2569cd6fea2d51c279c0166c0e3fb8fbc67
Nick Damiano <damiano.nick@gmail.com>
2015-03-08 22:03:39 -0500

    Adds method to loop through date range, scrape the pages, and build an array of comic hashes.

--- lib/scraper/comicScraper.rb
+++ lib/scraper/comicScraper.rb
@@ -5,7 +5,8 @@ require 'wombat'
 module PennyArcadeSearch
   class ComicScraper
     def self.scrapePage(comic_date)
-      parsed_date = comic_date.split('-')
+      # Convert date stamp to 2015-03-02
+      parsed_date = comic_date.to_s.split('-')
       year        = parsed_date[0]
       month       = parsed_date[1]
       day         = parsed_date[2]
@@ -25,12 +26,28 @@ module PennyArcadeSearch
       end
     end
 
-    def self.runScraper(start_date, end_date)
-      
+    # Builds an array of comic
+    #   date parameters are date stamps and to be compared
+    def self.run(start_date, end_date)
+      comics = []
+      date = start_date
+
+      while ( date < end_date )
+        result = scrapePage(date)
+
+        if result["headline"] != "404"
+          comics.push(result)
+        end
+        date = date + 1 
+      end 
+      comics 
     end
   end
 end
-test = PennyArcadeSearch::ComicScraper.scrapeComic('2015-02-02')
+today = Date.new(2015, 2, 15)
+end_date = Date.today
+test = PennyArcadeSearch::ComicScraper.run(today, end_date)
+# test = PennyArcadeSearch::ComicScraper.scrapePage(today + 1)
 p test
 # comic = Comic.create(img_url: test["img_url"])
 
