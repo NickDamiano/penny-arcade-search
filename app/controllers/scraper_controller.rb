16050a36e6bc02e4c2d6d07fa2de6182bf6d469e
Nick Damiano <damiano.nick@gmail.com>
2015-03-14 17:16:34 -0500

    Removes scraper controller

--- app/controllers/scraper_controller.rb
+++
@@ -1,7 +0,0 @@
-class ScraperController < ApplicationController
-  def index
-    today = Date.new(2015, 2, 15)
-    end_date = Date.today
-    test = PennyArcadeSearch::ComicScraper.run(today, end_date)
-  end
-end
