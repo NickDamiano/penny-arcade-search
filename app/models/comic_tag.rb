1e922de4579f706a7fffed16ab1145289d4c22d8
Nick Damiano <damiano.nick@gmail.com>
2015-12-30 21:00:58 -0800

    Displays comic title on tagger show page and adds todo code for comic_tag autosaving of comic title.

--- app/models/comic_tag.rb
+++ app/models/comic_tag.rb
@@ -7,6 +7,8 @@ class ComicTag < ActiveRecord::Base
     split_tags = tags.split(',').map{ |tag| tag.strip.downcase }
     split_tags.each do |tag|
       # Creates the tag and saves it - validation rejects duplicates
+      binding.pry
+      # TODO save title as a tag through the comictag and if tag being iterated through is same as title, do not save it and skip to next iteration.
       Tag.create(tagname: tag)
       tagname = Tag.find_by(tagname: tag)
       # As long as the record doesn't already exist, add it to the database
